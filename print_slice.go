231950acc405ebfc170555aca621870bda81b071
Florenzee <albertine.florenze@gmail.com>
2025-02-20 10:58:38 +0700

    first commit

---
+++ print_slice.go
@@ -0,0 +1,20 @@
+package go_print_slice
+
+import "fmt"
+
+func PrintSlice(slice []int) {
+	for i := 0; i < len(slice); i++ {
+		fmt.Println(slice[i], " ")
+	}
+
+}
+
+func PrintSlice2(slice [][]int) {
+	for i := 0; i < len(slice); i++ {
+		for j := i + 1; j < len(slice); j++ {
+			fmt.Print(slice[i][j], " ")
+		}
+		fmt.Println(slice[i], " ")
+	}
+
+}
