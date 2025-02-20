ed56fd7bbe46de81a9372bc632a9753f222e9d1d
Florenzee <albertine.florenze@gmail.com>
2025-02-20 13:36:14 +0700

    add function CreateSlice

--- print_slice.go
+++ print_slice.go
@@ -18,3 +18,12 @@ func PrintSlice2(slice [][]int) {
 	}
 	fmt.Println()
 }
+
+func CreateSlice(n int) (result [][]int) {
+	result = make([][]int, n)
+
+	for i := 0; i < n; i++ {
+		result[i] = make([]int, n)
+	}
+	return result
+}
